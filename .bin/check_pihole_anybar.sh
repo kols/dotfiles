9cd3637114c576f67814e185eca5c10289c3a3d0
Kane Dou <douqilong@gmail.com>
2019-01-04 19:31:47 +0800

    .bin, .service

---
+++ .bin/check_pihole_anybar.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -euo pipefail
+export SERVER=10.82.251.124
+export ANYBAR_PORT=1738
+open -a AnyBar
+if grep -q $SERVER /etc/resolv.conf && \
+    dig google.com @$SERVER +time=3 >/dev/null; then
+    echo -n green | nc -4u -w0 localhost $ANYBAR_PORT;
+else
+    echo -n red | nc -4u -w0 localhost $ANYBAR_PORT;
+fi
