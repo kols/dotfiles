abb0a9f90e4ffecab00238c0fcf79ed7670e6ff4
Kane Dou <douqilong@gmail.com>
2019-04-24 23:45:42 +0800

    emacs: update `default-black-theme'

--- .emacs.d/elisp/default-black-theme.el
+++ .emacs.d/elisp/default-black-theme.el
@@ -16,4 +16,9 @@
  '(show-paren-match ((nil (:background "#333399"))))
  '(show-paren-mismatch ((((class color)) (:background "red")))))
 
+;;;###autoload
+(when load-file-name
+  (add-to-list 'custom-theme-load-path
+               (file-name-as-directory (file-name-directory load-file-name))))
+
 (provide-theme 'default-black)
